f46987fdc6ee021dd22578aff05aa0fc4fc52b67
scottsak <48599109+scottsak@users.noreply.github.com>
2020-12-11 02:42:11 -0800

    learning how to create the board

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,27 @@
-import pygame as pg
+import pygame
+
+from pygame.locals import *
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+
+player = pygame.image.load("final shane poster.jpg")
 
 
 
+# 4 - keep looping through
+while 1:
+    # 5 - clear the screen before drawing it again
+    screen.fill(0)
+    # 6 - draw the screen elements
+    screen.blit(player, (100,100))
+    # 7 - update the screen
+    pygame.display.flip()
+    # 8 - loop through the events
+    for event in pygame.event.get():
+        # check if the event is the X button 
+        if event.type==pygame.QUIT:
+            # if it is quit the game
+            pygame.quit() 
+            exit(0) 
